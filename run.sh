53a2c5815f432ba80afdc5b2fd11a758aa7cb5bb
JiuhaiChen <jiuhaichen@gmail.com>
2021-09-12 16:43:09 +0800

    Delete run.sh
    delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-python experiments/run_expt.py --dataset ogb-molhiv  --algorithm groupDRO --model gcn  --root_dir data 
-
-python experiments/run_expt.py --dataset RotatedMNIST  --algorithm  groupDRO  --model gcn  --root_dir data  --frac 0.1
