d72e1ebd61bd9b815d82456377fec9ed195dc813
Ubuntu <ubuntu@ip-172-31-35-243.ap-northeast-1.compute.internal>
2021-09-12 08:46:42 +0000

    bash

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
-python experiments/run_expt.py --dataset ogb-molhiv  --algorithm groupDRO --model gcn  --root_dir data 
 
-python experiments/run_expt.py --dataset RotatedMNIST  --algorithm  groupDRO  --model gcn  --root_dir data  --frac 0.1
+
+for d in 'ogb-molhiv' 'RotatedMNIST' 
+    do
+        for m in  'gcn' 'gcn_virtual' 'gin' 'gin_virtual' 
+            do 
+                python experiments/run_expt.py --dataset ${d} --algorithm ERM --model ${m}  --root_dir data  --device 0 --eval_only True &
+                python experiments/run_expt.py --dataset ${d}  --algorithm deepCORAL --model ${m}  --root_dir data   --device 1  --eval_only True &
+                python experiments/run_expt.py --dataset ${d}   --algorithm groupDRO --model ${m}  --root_dir data   --device 2  --eval_only True&
+                python experiments/run_expt.py --dataset ${d}  --algorithm IRM --model ${m}  --root_dir data   --device 3  --eval_only True &
+                wait
+            done
+    done
+
+python experiments/run_expt.py --dataset ogb-molhiv --algorithm ERM --model gcn_virtual  --root_dir data  --device 0 
