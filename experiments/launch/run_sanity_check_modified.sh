1251d4e2a5c3847ddc0c13cbb46a8ee0e1cf491c
devin <devnkong@gmail.com>
2021-09-16 03:30:31 -0400

    sbatch

--- experiments/launch/run_sanity_check_modified.sh
+++ experiments/launch/run_sanity_check_modified.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
-#SBATCH --job-name=mle_single                           # sets the job name if not set from environment
+#SBATCH --job-name=graph-dg                           # sets the job name if not set from environment
 #SBATCH --array=0-119                                   # Submit 8 array jobs, throttling to 4 at a time
 #SBATCH --output slurm-logs/%x_%A_%a.log                # indicates a file to redirect STDOUT to; %j is the jobid, _%A_%a is array task id
 #SBATCH --error slurm-logs/%x_%A_%a.log                 # indicates a file to redirect STDERR to; %j is the jobid,_%A_%a is array task id
@@ -9,7 +9,7 @@
 #SBATCH --account=scavenger                             # set QOS, this will determine what resources can be requested
 #SBATCH --qos=scavenger                                 # set QOS, this will determine what resources can be requested
 #SBATCH --partition=scavenger
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:rtx2080ti:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem 32gb                                      # memory required by job; if unit is not specified MB will be assumed
 #SBATCH --nice=0
@@ -25,7 +25,7 @@ algorithm=${3}
 model=${4}
 root_dir=${5}
 
-log_path=${root_dir}/logs/error.txt
+log_path=${root_dir}/sanity_check_logs/error.txt
 
 (echoerr "Dataset: ${dataset}, Algorithm: ${algorithm}, Model: ${model}" && python run_expt.py --n_epochs 1 --device ${device} --dataset ${dataset}  --algorithm ${algorithm} --model ${model}  --root_dir ${root_dir}) 2> ${log_path}
 
@@ -37,24 +37,12 @@ device=0
 datasets=( ogb-molpcba ogb-molhiv ogbg-ppa RotatedMNIST ) #4
 algorithms=( ERM deepCORAL groupDRO IRM FLAG )            #5
 models=( gin gin_virtual gcn gcn_virtual cheb cheb_virtual ) #6
-root_dir=$(builtin cd ..; pwd)
+root_dir=/cmlscratch/kong/datasets/graph_domain
 
 dataset_idx=$(( ${SLURM_ARRAY_TASK_ID} % 4 ))
 algorithm_idx=$(( ${SLURM_ARRAY_TASK_ID} / 4 % 5 ))
 model_idx=$(( ${SLURM_ARRAY_TASK_ID} / 20 % 6 ))
 
 
-#device=0
-#datasets=( ogb-molpcba ogb-molhiv RotatedMNIST )
-#algorithms=( ERM deepCORAL groupDRO IRM )
-#models=( gin gin_virtual gcn gcn_virtual )
-#root_dir=/cmlscratch/kong/datasets/graph_domain
-#
-#dataset_idx=$(( ${SLURM_ARRAY_TASK_ID} % 3 ))
-#algorithm_idx=$(( ${SLURM_ARRAY_TASK_ID} / 3 % 4 ))
-#model_idx=$(( ${SLURM_ARRAY_TASK_ID} / 12 % 4 ))
-
-
 #runexp   device          dataset                   algorithm                       model            root_dir
 runexp  ${device}   ${datasets[$dataset_idx]}   ${algorithms[$algorithm_idx]}  ${models[$model_idx]}  ${root_dir}
-
