869d0e365efdee1a2c6c87b66f2cbf52ae2022b8
Ubuntu <ubuntu@ip-172-31-35-243.ap-northeast-1.compute.internal>
2021-09-21 03:05:20 +0000

    upfd

--- experiments/models/initializer.py
+++ experiments/models/initializer.py
@@ -1,12 +1,7 @@
 import torch.nn as nn
 from torch_geometric.nn import global_mean_pool
 
-<<<<<<< HEAD
-
-def initialize_model(config, d_out, full_dataset=None, is_featurizer=False):
-=======
 def initialize_model(config, d_out, is_featurizer=False, is_pooled=True):
->>>>>>> master
     """
     Initializes models according to the config
         Args:
@@ -22,28 +17,6 @@ def initialize_model(config, d_out, is_featurizer=False, is_pooled=True):
             - model: a model that is equivalent to nn.Sequential(featurizer, classifier)
     """
 
-<<<<<<< HEAD
-    if config.gsn == True:
-        from models.gsn.gnn import GNN_OGB
-        model = GNN_OGB(in_features=full_dataset.num_features,
-                out_features=d_out, 
-                encoder_ids=full_dataset.encoder_ids,
-                d_in_id=full_dataset.d_id,
-                in_edge_features=full_dataset.num_edge_features, 
-                d_in_node_encoder=full_dataset.d_in_node_encoder,
-                d_in_edge_encoder=full_dataset.d_in_edge_encoder,
-                d_degree=full_dataset.d_degree)
-    else:
-        from models.gnn import GNN
-        if is_featurizer:
-            featurizer = GNN(gnn_type=config.model, num_tasks=None, **config.model_kwargs)
-            classifier = nn.Linear(featurizer.d_out, d_out)
-            model = (featurizer, classifier)
-        else:
-            model = GNN(gnn_type=config.model, num_tasks=d_out, **config.model_kwargs)
-
-
-=======
 
     from models.gnn import GNN
     if is_featurizer:
@@ -58,7 +31,6 @@ def initialize_model(config, d_out, is_featurizer=False, is_pooled=True):
             model = (featurizer, pooler, classifier)
     else:
         model = GNN(gnn_type=config.model, num_tasks=d_out, is_pooled=is_pooled, **config.model_kwargs)
->>>>>>> master
 
 
     # The `needs_y` attribute specifies whether the model's forward function
