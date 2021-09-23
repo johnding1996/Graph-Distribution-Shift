b7e504316fd869013bea82dac5235d1a1ebd437a
Mucong Ding <mcding@umd.edu>
2021-09-22 22:38:52 -0400

    fix the matplotlib dev dep and gds package version, half way revising SBM generators

--- gds/version.py
+++ gds/version.py
@@ -1,10 +1,8 @@
-# Adapted from https://github.com/snap-stanford/ogb/blob/master/ogb/version.py
-
 import logging
 import os
 from threading import Thread
 
-__version__ = '1.2.2'
+__version__ = '0.1.0'
 
 try:
     os.environ['OUTDATED_IGNORE'] = '1'
@@ -18,7 +16,7 @@ def check():
         is_outdated, latest = check_outdated('gds', __version__)
         if is_outdated:
             logging.warning(
-                f'The WILDS package is out of date. Your version is '
+                f'The GDS package is out of date. Your version is '
                 f'{__version__}, while the latest version is {latest}.')
     except Exception:
         pass
